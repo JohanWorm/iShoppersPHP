493d50f8db2bb7cede2fb55b29513c5fb09e56a5
Johan Eduardo Cuberos Grisales <johan.worm@gmail.com>
2020-06-12 21:35:27 -0500

    Add view nav and carousel

---
+++ common/carousel.php
@@ -0,0 +1,35 @@
+<div id="carouselAdvertisements" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselAdvertisements" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselAdvertisements" data-slide-to="1"></li>
+    <li data-target="#carouselAdvertisements" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./assets/img/advertisements1.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Productos de calidad</h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./assets/img/advertisements2.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Entregas el mismo día</h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./assets/img/advertisements3.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Compras seguras</h5>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselAdvertisements" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselAdvertisements" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
